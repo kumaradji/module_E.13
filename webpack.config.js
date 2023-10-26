a2a036dfc22d22d79f49619f1d9eab80471e272b
kumaradji <kumaradji@gmail.com>
2023-10-26 21:15:19 +0300

    server OK

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,36 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devServer: {
-    static: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    }),
-  ],
-  output: {
-    filename: 'main.js',
-  },
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode === 'development';
+
+  return {
+    mode: isDevelopment ? 'development' : 'production',
+    entry: './src/index.js',
+    devServer: {
+      static: './dist',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'My App',
+        template: './src/index.html',
+        filename: 'index.html',
+      }),
+      new MiniCssExtractPlugin(), // MiniCssExtractPlugin instance
+    ],
+    output: {
+      filename: isDevelopment ? 'main.js' : 'main.[contenthash].js',
+    },
+  };
 };
