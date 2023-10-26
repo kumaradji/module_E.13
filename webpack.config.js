b08d26168371377f585bde017eb08db6dfe959c5
kumaradji <kumaradji@me.com>
2023-10-26 18:38:19 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+  },
+};
