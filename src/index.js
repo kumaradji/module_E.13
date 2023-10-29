7cb09f33dd299ca1c4a6e288397ac2e25ded14cc
kumaradji <kumaradji@gmail.com>
2023-10-29 09:58:03 +0300

    husky + lint

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './main.css';
 
 // Создаем элемент для текста
 const textElement = document.createElement('div');
-textElement.textContent = 'Hello E13!';
+textElement.textContent = 'Приветствую, человек! Это Module E13!';
 
 // Добавляем созданный элемент в корневой элемент <div id="app"> на вашей странице
 document.getElementById('app').appendChild(textElement);
